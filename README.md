a5244dc039f9efe977903f54358c04e891ed8c8b
actionchain <actionchain@chainguard.dev>
2026-06-11 11:08:59 +0000

    Update git-for-windows/setup-git-for-windows-sdk@v1.11.0

--- README.md
+++ README.md
@@ -1,6 +1,124 @@
-# git-for-windows/setup-git-for-windows-sdk
+# Set up a Git for Windows SDK (or a subset thereof)
 
-Hardened GitHub Action by [Chainguard](https://www.chainguard.dev).
+Use this Action to initialize an environment to develop Git for Windows.
+
+## Getting Started
+
+```yaml
+name: Build stuff in Git for Windows' SDK
+on: [push]
+jobs:
+  build:
+    runs-on: windows-latest
+    steps:
+    - uses: actions/checkout@v3
+    - name: Setup Git for Windows' minimal SDK
+      uses: git-for-windows/setup-git-for-windows-sdk@v1
+    - name: Build
+      shell: bash
+      run: make
+```
+
+## Input parameters
+
+### Available flavors
+
+This Action supports several flavors (read: subsets) of the Git for Windows SDK that can be configured like this:
+
+```yaml
+- uses: git-for-windows/setup-git-for-windows-sdk
+  with:
+    flavor: build-installers
+```
+
+The supported flavors are:
+
+- `minimal`:
+
+  This is the most useful flavor to build Git for Windows' source code and run its own test suite. Only available for x86_64.
+
+- `makepkg-git`:
+
+  This flavor allows packaging `mingw-w64-git`, the Pacman package. It is only available for x86_64 but can be used to "cross-compile" for i686.
+
+- `build-installers`:
+
+  In addition to building `mingw-w64-git`, this flavor allows bundling Git for Windows' artifacts such as the installer and the Portable Git.
+
+- `full`:
+
+  This is the "full" SDK, [as users would install it](https://gitforwindows.org/#download-sdk), with a pre-selected set of packages pre-installed. Additional packages can be installed via `pacman -S <package>`.
+
+### CPU architecture support
+
+Git for Windows SDK comes in variants targeting `x86_64` (AKA "64-bit"), `i686` (AKA 32-bit) and  `aarch64` (AKA arm64). The default is `x86_64` and can be overridden like this:
+
+```yaml
+- uses: git-for-windows/setup-git-for-windows-sdk
+  with:
+    flavor: build-installers
+    architecture: i686
+```
+
+Please note that only the `build-installers` and the `full` flavors are available for `i686`.
+
+### Verbosity
+
+By default, this Action prints a line whenever 250 items were extracted (this does not work for the `full` flavor, where this Action is silent by default). It can be overridden by setting the input parameter `verbose`; setting it to a number will show updates whenever that many items were extracted. Setting it to `false` will suppress progress updates. Setting it to `true` will print every extracted file (this also works for the `full` flavor).
+
+### Caching
+
+To accelerate this Action, artifacts are cached once downloaded. This can be turned off by setting the input parameter `cache` to `false`.
+
+In practice, caching the `full` artifacts does not provide much of a speed-up. Instead, it slows it down by spending extra minutes on caching the artifact. Therefore, caching is disabled for the `full` artifacts by default, corresponding to `cache: auto`.
+
+### Clean-up
+
+On self-hosted runners, the SDK files persist after the workflow run is done. To remove these files, set the input parameter `cleanup` to `true`.
+
+## Developing _this_ Action
+
+> First, you'll need to have a reasonably modern version of `node` handy, such as Node 12.
+
+Install the dependencies
+
+```bash
+$ npm install
+```
+
+Build the Action and package it for distribution
+
+```bash
+$ npm run build && npm run package
+```
+
+Run the tests :heavy_check_mark:
+
+```bash
+$ npm test
+
+> setup-git-for-windows-sdk@0.0.0 test C:\Users\me\setup-git-for-windows-sdk
+> jest
+
+PASS __tests__/main.test.ts (28.869 s)
+  √ skipping tests requiring network access (224 ms)
+
+  console.log
+    If you want to run tests that access the network, set:
+    export RUN_NETWORK_TESTS=true
+
+      at __tests__/main.test.ts:26:13
+
+PASS __tests__/downloader.test.ts (29.889 s)
+  √ can obtain build ID (9 ms)
+
+Test Suites: 2 passed, 2 total
+Tests:       2 passed, 2 total
+Snapshots:   0 total
+Time:        31.11 s
+Ran all test suites.
+...
+```
 
 ## Privacy
 
---
+++ src/git.ts
@@ -0,0 +1,241 @@
+import * as core from '@actions/core'
+import {ChildProcess, spawn} from 'child_process'
+import {Octokit} from '@octokit/rest'
+import {delimiter} from 'path'
+import * as fs from 'fs'
+
+// If present, do prefer the build agent's copy of Git
+const externalsGitDir = `${process.env.AGENT_HOMEDIRECTORY}/externals/git`
+const gitForWindowsRoot = 'C:/Program Files/Git'
+const gitRoot = fs.existsSync(externalsGitDir)
+  ? externalsGitDir
+  : gitForWindowsRoot
+
+const gitForWindowsBinPaths = ['clangarm64', 'mingw64', 'mingw32', 'usr'].map(
+  p => `${gitRoot}/${p}/bin`
+)
+export const gitForWindowsUsrBinPath =
+  gitForWindowsBinPaths[gitForWindowsBinPaths.length - 1]
+const gitExePath = `${gitRoot}/cmd/git.exe`
+
+/*
+ * It looks a bit ridiculous to use 56 workers on a build agent that has only
+ * a two-core CPU, yet manual testing revealed that 64 workers would be _even
+ * better_. But at 92 workers, resources are starved so much that the checkout
+ * is not only much faster, but also fails.
+ *
+ * Let's stick with 56, which should avoid running out of resources, but still
+ * is much faster than, say, using only 2 workers.
+ */
+const GIT_CONFIG_PARAMETERS = `'checkout.workers=56'`
+
+export function getArtifactMetadata(
+  flavor: string,
+  architecture: string
+): {repo: string; artifactName: string} {
+  const repo = {
+    i686: 'git-sdk-32',
+    x86_64: 'git-sdk-64',
+    aarch64: 'git-sdk-arm64'
+  }[architecture]
+
+  if (repo === undefined) {
+    throw new Error(`Invalid architecture ${architecture} specified`)
+  }
+
+  const artifactName = `${repo}-${flavor}`
+
+  return {repo, artifactName}
+}
+
+async function clone(
+  url: string,
+  destination: string,
+  verbose: number | boolean,
+  cloneExtraOptions: string[] = []
+): Promise<void> {
+  if (verbose) core.info(`Cloning ${url} to ${destination}`)
+  const child = spawn(
+    gitExePath,
+    [
+      'clone',
+      '--depth=1',
+      '--single-branch',
+      '--branch=main',
+      ...cloneExtraOptions,
+      url,
+      destination
+    ],
+    {
+      env: {
+        GIT_CONFIG_PARAMETERS
+      },
+      stdio: [undefined, 'inherit', 'inherit']
+    }
+  )
+  return new Promise<void>((resolve, reject) => {
+    child.on('close', code => {
+      if (code === 0) {
+        resolve()
+      } else {
+        reject(new Error(`git clone: exited with code ${code}`))
+      }
+    })
+  })
+}
+
+async function updateHEAD(
+  bareRepositoryPath: string,
+  headSHA: string
+): Promise<void> {
+  const child = spawn(
+    gitExePath,
+    ['--git-dir', bareRepositoryPath, 'update-ref', 'HEAD', headSHA],
+    {
+      env: {
+        GIT_CONFIG_PARAMETERS
+      },
+      stdio: [undefined, 'inherit', 'inherit']
+    }
+  )
+  return new Promise<void>((resolve, reject) => {
+    child.on('close', code => {
+      if (code === 0) {
+        resolve()
+      } else {
+        reject(new Error(`git: exited with code ${code}`))
+      }
+    })
+  })
+}
+
+export async function getViaGit(
+  flavor: string,
+  architecture: string,
+  githubToken?: string
+): Promise<{
+  artifactName: string
+  id: string
+  download: (
+    outputDirectory: string,
+    verbose?: number | boolean
+  ) => Promise<void>
+}> {
+  const owner = 'git-for-windows'
+
+  const {repo, artifactName} = getArtifactMetadata(flavor, architecture)
+
+  const octokit = githubToken ? new Octokit({auth: githubToken}) : new Octokit()
+  let head_sha: string
+  if (flavor === 'minimal') {
+    const info = await octokit.actions.listWorkflowRuns({
+      owner,
+      repo,
+      workflow_id: 938271,
+      status: 'success',
+      branch: 'main',
+      event: 'push',
+      per_page: 1
+    })
+    head_sha = info.data.workflow_runs[0].head_sha
+    /*
+     * There was a GCC upgrade to v14.1 that broke the build with `DEVELOPER=1`,
+     * and `ci-artifacts` was not updated to test-build with `DEVELOPER=1` (this
+     * was fixed in https://github.com/git-for-windows/git-sdk-64/pull/83).
+     *
+     * Work around that by forcing the incorrectly-passing revision back to the
+     * last one before that GCC upgrade.
+     */
+    if (head_sha === '5f6ba092f690c0bbf84c7201be97db59cdaeb891') {
+      head_sha = 'e37e3f44c1934f0f263dabbf4ed50a3cfb6eaf71'
+    }
+  } else {
+    const info = await octokit.repos.getBranch({
+      owner,
+      repo,
+      branch: 'main'
+    })
+    head_sha = info.data.commit.sha
+  }
+  const id = `${artifactName}-${head_sha}${head_sha === 'e37e3f44c1934f0f263dabbf4ed50a3cfb6eaf71' ? '-2' : ''}`
+  core.info(`Got commit ${head_sha} for ${repo}`)
+
+  return {
+    artifactName,
+    id,
+    download: async (
+      outputDirectory: string,
+      verbose: number | boolean = false
+    ): Promise<void> => {
+      core.startGroup(`Cloning ${repo}`)
+      const partialCloneArg = flavor === 'full' ? [] : ['--filter=blob:none']
+      await clone(`https://github.com/${owner}/${repo}`, `.tmp`, verbose, [
+        '--bare',
+        ...partialCloneArg
+      ])
+      core.endGroup()
+
+      let child: ChildProcess
+      if (flavor === 'full') {
+        core.startGroup(`Checking out ${repo}`)
+        child = spawn(
+          gitExePath,
+          [`--git-dir=.tmp`, 'worktree', 'add', outputDirectory, head_sha],
+          {
+            env: {
+              GIT_CONFIG_PARAMETERS
+            },
+            stdio: [undefined, 'inherit', 'inherit']
+          }
+        )
+      } else {
+        await updateHEAD('.tmp', head_sha)
+        core.startGroup('Cloning build-extra')
+        await clone(
+          `https://github.com/${owner}/build-extra`,
+          '.tmp/build-extra',
+          verbose
+        )
+        core.endGroup()
+
+        core.startGroup(`Creating ${flavor} artifact`)
+        const traceArg = verbose ? ['-x'] : []
+        child = spawn(
+          `${gitForWindowsUsrBinPath}/bash.exe`,
+          [
+            ...traceArg,
+            '.tmp/build-extra/please.sh',
+            'create-sdk-artifact',
+            `--architecture=${architecture}`,
+            `--out=${outputDirectory}`,
+            '--sdk=.tmp',
+            flavor
+          ],
+          {
+            env: {
+              GIT_CONFIG_PARAMETERS,
+              COMSPEC:
+                process.env.COMSPEC ||
+                `${process.env.WINDIR}\\system32\\cmd.exe`,
+              LC_CTYPE: 'C.UTF-8',
+              CHERE_INVOKING: '1',
+              MSYSTEM: 'MINGW64',
+              PATH: `${gitForWindowsBinPaths.join(delimiter)}${delimiter}${process.env.PATH}`
+            },
+            stdio: [undefined, 'inherit', 'inherit']
+          }
+        )
+      }
+      return new Promise<void>((resolve, reject) => {
+        child.on('close', code => {
+          core.endGroup()
+          if (code === 0) {
+            fs.rm('.tmp', {recursive: true}, () => resolve())
+          } else {
+            reject(new Error(`process exited with code ${code}`))
+          }
+        })
+      })
+    }
+  }
+}
